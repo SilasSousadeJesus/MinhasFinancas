67e872961333a9b1e3736bf119e962c61276039b
silassousa <silassousadejesus@gmail.com>
2024-07-18 12:17:37 -0300

    implementando metodo de cadastrar usuarios e metodo de login (ainda a ser finalizado)

--- minhas-financas-back-end/MinhasFinancas.Application/Services/AutenticacaoAppService.cs
+++ minhas-financas-back-end/MinhasFinancas.Application/Services/AutenticacaoAppService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using MinhasFinancas.Application.DTOs;
 using MinhasFinancas.Application.Interfaces;
 
@@ -16,22 +17,47 @@ namespace MinhasFinancas.Application.Services
             _userManager = userManager;    
         }
 
-        public Task<RetornoGenerico> Cadastrar(CadastroUsuarioDTO cadastroUsuarioDTO)
+        public async Task<RetornoGenerico> Cadastrar(CadastroUsuarioDTO cadastroUsuarioDTO)
         {
+
             var identityUser = new IdentityUser
             {
                 UserName = cadastroUsuarioDTO.Email,
                 Email = cadastroUsuarioDTO.Email,
                 EmailConfirmed = true
             };
-               
 
-            throw new NotImplementedException();
+            var result = await _userManager.CreateAsync(identityUser, cadastroUsuarioDTO.Senha);
+            if (result.Succeeded) await _userManager.SetLockoutEnabledAsync(identityUser, false);
+
+            var mensagem = result.Succeeded ? "Usuario criado com sucesso" : "Usuario não pode ser criado";
+            var statusCode = result.Succeeded ? System.Net.HttpStatusCode.Created : System.Net.HttpStatusCode.BadRequest;
+
+            return new RetornoGenerico(result.Succeeded, mensagem, mensagem, statusCode);
         }
 
-        public Task<RetornoGenerico> Login(LoginDTO loginDTO)
+        public async Task<RetornoGenerico> Login(LoginDTO loginDTO)
         {
-            throw new NotImplementedException();
+            var resultado = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Senha, false, true);
+
+            var mensagem = resultado.Succeeded ? "Login efetuado com sucesso" :
+                           resultado.IsLockedOut ? "Esta Conta está bloqueada" :
+                           resultado.IsNotAllowed ? "Esta Conta não tem permissão para fazer login" :
+                           resultado.RequiresTwoFactor ? "É necessário confirmar o login no seu email" :
+                           "Erro ao tentar efetuar o login";
+
+            //var token = resultado.Succeeded ? await GerarRoken(loginDTO.Email) : null;
+            string token =  null;
+
+            return new RetornoGenerico
+            {
+                Sucesso = resultado.Succeeded,
+                HttpStatusCode = resultado.Succeeded ? System.Net.HttpStatusCode.OK : System.Net.HttpStatusCode.Unauthorized,
+                MensagemSistema = mensagem,
+                MensagemUsuario = mensagem,
+                Dados = token
+            };
         }
+
     }
 }
