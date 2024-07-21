5d8eefa3dee1329c2f8a8d1a7a369ad65aee6928
silassousa <silassousadejesus@gmail.com>
2024-07-21 16:45:38 -0300

    criando a classe de valor dashboard

---
+++ minhas-financas-back-end/MinhasFinancas.Domain/Services/DashBoard.cs
@@ -0,0 +1,53 @@
+﻿using MinhasFinancas.Domain.Entities;
+
+namespace MinhasFinancas.Domain.Services
+{
+    public class Dashboard
+    {
+        public Dashboard() { }
+
+        public Dashboard(List<Lancamento> listaLancamentos)
+        {
+            CalcularReceitas(listaLancamentos);
+        }
+
+        public string ReceitaAnoCorrente { get; set; } = string.Empty;
+        public string ReceitaMesCorrente { get; set; } = string.Empty;
+        public string ReceitaMesPassado { get; set; } = string.Empty;
+
+
+
+
+
+
+
+
+
+
+        public void CalcularReceitas(List<Lancamento> listaLancamentos)
+        {
+            // Implementar a lógica de cálculo aqui
+            ReceitaAnoCorrente = CalcularReceitaAnoCorrente(listaLancamentos);
+            ReceitaMesCorrente = CalcularReceitaMesCorrente(listaLancamentos);
+            ReceitaMesPassado = CalcularReceitaMesPassado(listaLancamentos);
+        }
+
+        private string CalcularReceitaAnoCorrente(List<Lancamento> listaLancamentos)
+        {
+            // Lógica para calcular a receita do ano corrente
+            return "Resultado do Ano Corrente";
+        }
+
+        private string CalcularReceitaMesCorrente(List<Lancamento> listaLancamentos)
+        {
+            // Lógica para calcular a receita do mês corrente
+            return "Resultado do Mês Corrente";
+        }
+
+        private string CalcularReceitaMesPassado(List<Lancamento> listaLancamentos)
+        {
+            // Lógica para calcular a receita do mês passado
+            return "Resultado do Mês Passado";
+        }
+    }
+}
