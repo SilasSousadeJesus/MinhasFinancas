908e7dba6e2204c3993b9992b67a64c1fce8fef9
silassousa <silassousadejesus@gmail.com>
2024-07-23 11:39:26 -0300

    add futura classe para analise de patrimonio

--- minhas-financas-back-end/minhas-financas-back-end/Program.cs
+++ minhas-financas-back-end/minhas-financas-back-end/Program.cs
@@ -21,7 +21,7 @@ namespace minhas_financas_back_end
         {
             // criar logicar para desativar os lançamentos fixos e parcelados caso o cara tente realizar o lancamento
             // criar logica para ter registros de saque e deposito (movimentação)
-
+            // criar sistema de metas/sonhos/objetivos estilo caixinha do nubank
 
             var builder = WebApplication.CreateBuilder(args);
 
