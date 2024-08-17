07649695d6e2b21e6385995631ca07b5da919b86
silassousa <silassousadejesus@gmail.com>
2024-08-17 17:03:04 -0300

    ajustando layout do dashboard

--- minhas-financas-front-end/src/app/(authenticated)/dashboard/page.tsx
+++ minhas-financas-front-end/src/app/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import { PainelDashboard } from "@/components/dashboard/dashboard";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
+
 export default function Dashboard() {
   return (
-      <h1>DASHBOARD</h1>
+    <div className="flex flex-row">
+      <Sidebar />
+      <PainelDashboard/>
+    </div>
   );
 }
