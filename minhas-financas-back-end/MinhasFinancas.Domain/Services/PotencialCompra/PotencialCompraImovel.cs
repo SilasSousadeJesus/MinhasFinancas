9514495984a2e2d061e1a2f1b10d909ce022995e
silassousa <silassousadejesus@gmail.com>
2024-12-31 16:13:10 -0300

    adicionando função de sorteio de jogos

--- minhas-financas-back-end/MinhasFinancas.Domain/Services/PotencialCompra/PotencialCompraImovel.cs
+++ minhas-financas-back-end/MinhasFinancas.Domain/Services/PotencialCompra/PotencialCompraImovel.cs
@@ -2,57 +2,35 @@
 {
     public class PotencialCompraImovel
     {
-        public decimal RendaMensal { get; private set; }
-        public decimal EntradaFGTS { get; private set; }
-        public decimal ValorImovel { get; private set; }
-        public decimal ValorFinanciado { get; private set; }
-        public decimal ITBIOutrasTaxas { get; private set; }
-        public decimal ValorTotalComTaxas { get; private set; }
+        public decimal RendaMensal { get; set; }
+        public decimal Entrada { get; set; }
+        public int Prazo { get; set; } // em anos
+        public decimal ValorFinanciado { get; set; } // em anos
+        public decimal PotencialCompra { get; set; } // em anos
 
-
-        public PotencialCompraImovel()
-        {
-
-        }
-
-        public PotencialCompraImovel(decimal rendaMensal, decimal entradaFGTS)
+        public PotencialCompraImovel(decimal rendaMensal, decimal entrada, int prazo = 30)
         {
             RendaMensal = rendaMensal;
-            EntradaFGTS = entradaFGTS;
-            CalcularPotencialDeCompra();
+            Entrada = entrada;
+            Prazo = prazo;
+
+            ValorFinanciado = CalcularValorFinanciado();
+            PotencialCompra = CalcularPotencialCompra();
         }
 
-        private void CalcularPotencialDeCompra()
+        public decimal CalcularValorFinanciado()
         {
-            // 1. Calcula o valor máximo da parcela (30% da renda mensal)
-            decimal valorMaximoParcela = RendaMensal * 0.30m; // 30% da renda mensal
-
-            // 2. Estima o valor financiável com base na parcela máxima
-            int prazoMeses = 360; 
-
-            decimal valorFinanciadoMaximo = Math.Round(valorMaximoParcela * prazoMeses);
-
-            // 3. Calcula o valor do imóvel 
- 
-            ValorImovel = valorFinanciadoMaximo;
-
-            // valor financiado para ser a diferença entre o valor do imóvel e a entrada
-            ValorFinanciado = valorFinanciadoMaximo - EntradaFGTS;
-
-            // 4. Calcula ITBI e outras taxas (5% do valor do imóvel)
-            decimal porcentagemTaxas = 0.05m;
-            ITBIOutrasTaxas = ValorImovel * porcentagemTaxas;
-
-            // 5. Valor total com taxas
-            ValorTotalComTaxas = ValorImovel + ITBIOutrasTaxas;
+            // Supondo que a parcela não pode comprometer mais que 30% da renda mensal
+            decimal parcelaMaxima = RendaMensal * 0.30m;
+            int numeroParcelas = Prazo * 12;
+            decimal valorFinanciado = parcelaMaxima * numeroParcelas;
+            return valorFinanciado;
         }
 
-        public void ExibirResultado()
+        public decimal CalcularPotencialCompra()
         {
-            Console.WriteLine("Entrada: R$ " + EntradaFGTS.ToString("N2"));
-            Console.WriteLine("Valor a financiar: R$ " + ValorFinanciado.ToString("N2"));
-            Console.WriteLine("ITBI e outras taxas: R$ " + ITBIOutrasTaxas.ToString("N2"));
-            Console.WriteLine("Valor total do imóvel com taxas: R$ " + ValorTotalComTaxas.ToString("N2"));
+            decimal valorFinanciado = CalcularValorFinanciado();
+            return Entrada + valorFinanciado;
         }
     }
 }
---
+++ minhas-financas-back-end/MinhasFinancas.Domain/Services/Sorteios/Sorteios.cs
@@ -0,0 +1,24 @@
+﻿namespace MinhasFinancas.Domain.Services.Sorteios
+{
+    public class Sorteios
+    {
+        public Sorteios() { }
+
+        public int[] MegaSena(int quantidade, int min, int max)
+        {
+            return Sorteio(6, 1, 60);
+        }
+
+
+        private int[] Sorteio(int quantidade, int min, int max)
+        {
+            Random random = new Random();
+            return Enumerable.Range(min, max - min + 1)
+                             .OrderBy(_ => random.Next())
+                             .Take(quantidade)
+                             .ToArray();
+        }
+    }
+}
+
+
